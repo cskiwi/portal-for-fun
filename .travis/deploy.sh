e5d871fe608a4937c40bc677f4f71b6736f1a732
SIDGLAT <SIDGLAT@WS33177>
2016-09-02 10:28:23 +0200

    update script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ '$TRAVIS_BRANCH' == 'master' ] then
-  echo "Deploying!"
-  cd Web
-  firebase deploy --token $FIREBASE_TOKEN
-else
-  echo "Succesfull build!"
-fi
+echo "Deploying!"
+cd Web
+firebase deploy --token $FIREBASE_TOKEN
